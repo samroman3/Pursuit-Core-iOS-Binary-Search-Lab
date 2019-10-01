344e9fd48ab753b867ff7c6dde3dbf95c0de00e3
Ben Stone <benjamin.h.stone@gmail.com>
2019-10-01 13:01:25 -0400

    add first three tests

--- BinarySearchProblems/BinarySearchProblemsTests/BinarySearchProblemsTests.swift
+++ BinarySearchProblems/BinarySearchProblemsTests/BinarySearchProblemsTests.swift
@@ -11,24 +11,84 @@ import XCTest
 
 class BinarySearchProblemsTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    struct MatrixTestCase<T, ExpectedValue: Equatable> {
+        let arr: [[T]]
+        let target: T
+        let expectedValue: ExpectedValue
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    struct TestCase<T, ExpectedValue: Equatable> {
+        let arr: [T]
+        let target: T
+        let expectedValue: ExpectedValue
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testBinarySearch() {
+        let doubleTestCases: [TestCase<Double, Bool>] = [TestCase(arr: [1,2,3,4,5,6.0], target: 4.0, expectedValue: true),
+                                                   TestCase(arr: [0.2, 0.4, 0.45], target: 0.4, expectedValue: true),
+                                                   TestCase(arr: [-3, -1, 0, 2], target: 1, expectedValue: false)
+        ]
+        
+        let stringTestCases: [TestCase<String, Bool>] = [TestCase(arr: ["a", "b", "d", "e"], target: "c", expectedValue: false),
+                                                   TestCase(arr: ["a", "b", "d", "e"], target: "d", expectedValue: true)
+        ]
+        
+        for testCase in doubleTestCases {
+            XCTAssertEqual(binarySearch(arr: testCase.arr, target: testCase.target), testCase.expectedValue)
+        }
+        
+        for testCase in stringTestCases {
+            XCTAssertEqual(binarySearch(arr: testCase.arr, target: testCase.target), testCase.expectedValue)
         }
     }
-
+    
+    func testRecursiveBinarySearch() {
+        let doubleTestCases: [TestCase<Double, Bool>] = [TestCase(arr: [1,2,3,4,5,6.0], target: 4.0, expectedValue: true),
+                                                   TestCase(arr: [0.2, 0.4, 0.45], target: 0.4, expectedValue: true),
+                                                   TestCase(arr: [-3, -1, 0, 2], target: 1, expectedValue: false)
+        ]
+        
+        let stringTestCases: [TestCase<String, Bool>] = [TestCase(arr: ["a", "b", "d", "e"], target: "c", expectedValue: false),
+                                                   TestCase(arr: ["a", "b", "d", "e"], target: "d", expectedValue: true)
+        ]
+        
+        for testCase in doubleTestCases {
+            XCTAssertEqual(recursiveBinarySearch(arr: testCase.arr, target: testCase.target), testCase.expectedValue)
+        }
+        
+        for testCase in stringTestCases {
+            XCTAssertEqual(recursiveBinarySearch(arr: testCase.arr, target: testCase.target), testCase.expectedValue)
+        }
+    }
+    
+    func testMatrixContains() {
+        let matrixTestCases: [MatrixTestCase<Int, Bool>] = [MatrixTestCase(arr: [[1,   3,  5,  7],
+                                                                           [10, 11, 16, 20],
+                                                                           [23, 30, 34, 50]],
+                                                                     target: 3, expectedValue: true),
+                                                            MatrixTestCase(arr: [[1,   3,  5,  7],
+                                                                           [10, 11, 16, 20],
+                                                                           [23, 30, 34, 50]],
+                                                                     target: 34, expectedValue: true),
+                                                            MatrixTestCase(arr: [[1,   3,  5,  7],
+                                                                           [10, 11, 16, 20],
+                                                                           [23, 30, 34, 50]],
+                                                                     target: 31, expectedValue: false),
+                                                            MatrixTestCase(arr: [[1, 5, 8, 12],
+                                                                           [13, 18, 22, 800]],
+                                                                     target: 1, expectedValue: true),
+                                                            MatrixTestCase(arr: [[1, 5, 8, 12],
+                                                                           [13, 18, 22, 800]],
+                                                                     target: 800, expectedValue: true),
+                                                            MatrixTestCase(arr: [[1, 5, 8, 12],
+                                                                           [13, 18, 22, 800]],
+                                                                     target: 18, expectedValue: true),
+                                                            MatrixTestCase(arr: [[1, 5, 8, 12],
+                                                                           [13, 18, 22, 800]],
+                                                                     target: 30, expectedValue: false),
+        ]
+        for testCase in matrixTestCases {
+            XCTAssertEqual(contains(value: testCase.target, in: testCase.arr), testCase.expectedValue)
+        }
+    }
 }
