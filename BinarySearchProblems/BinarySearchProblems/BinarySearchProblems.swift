de9b6ffbdb3c83bcb3e84f83cea6d14f0bc75318
Ben Stone <benjamin.h.stone@gmail.com>
2019-10-01 12:22:44 -0400

    initial commit

---
+++ BinarySearchProblems/BinarySearchProblems/BinarySearchProblems.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+//
+
+// 1. Find if an element is contained within a sorted array in O(log(n)) time.  Do not use recursion.
+
+func binarySearch<T: Comparable>(arr: [T], target: T) -> Bool {
+    return false
+}
+
+// 2. Find if an element is contained within a sorted array in O(log(n)) time.  Use recursion.
+
+func recursiveBinarySearch<T: Comparable>(arr: [T], target: T) -> Bool {
+    return false
+}
+
+// https://www.interviewbit.com/problems/matrix-search/
+
+// 3. Find if a value is contained in a matrix of dimensions m x n. This matrix has the following properties:
+
+// Integers in each row are sorted from left to right.
+// The first integer of each row is greater than or equal to the last integer of the previous row.
+
+func contains(value: Int, in arr: [[Int]]) -> Bool {
+    return false
+}
+
+
+
+// https://www.techiedelight.com/find-first-or-last-occurrence-of-a-given-number-sorted-array/
+// 4. Given a sorted array of integers, find the index of the last occurrence of a given number.  Your solution should work in O(log(n)) time
+
+func lastOccurrence<T: Comparable>(of value: T, in arr: [T]) -> Int? {
+    return nil
+}
+
+// https://www.techiedelight.com/find-smallest-missing-element-sorted-array/
+// 5. Given a sorted array of distinct non-negative integers, find the smallest missing number.  Your solution should work in O(log(n)) time
+
+func smallestMissingNumber(in arr: [Int]) -> Int? {
+    return nil
+}
